bb2fa42b86577f179fdc0d8e6e123d795d74723d
Co-lead <cojohndel53@gmail.com>
2025-05-04 11:19:08 +0800

    initial commit

---
+++ projects.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Projects - My Personal Site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Projects</h1>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="about.html">About Me</a>
+            <a href="projects.html">Projects</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+    </header>
+
+    <div class="container">
+        <h2>My Projects</h2>
+        <p>Here are some of the projects I've worked on:</p>
+        <ul>
+            <li>Project 1 - Shuuji</li>
+            <li>Project 2 - StuTrack</li>
+            <li>Project 3 - Personal Website</li>
+            <li>Project 4 - Ongoing project Labmate</li>
+        </ul>
+    </div>
+</body>
+</html>
